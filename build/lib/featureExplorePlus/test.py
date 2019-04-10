cfad0deaacad3e1fa5bb6fb0fed0004f786bf382
xiaolin <xlzhong123@163.com>
2019-04-10 18:46:40 +0800

    add examples

--- build/lib/featureExplorePlus/test.py
+++ build/lib/featureExplorePlus/test.py
@@ -1,12 +1,13 @@
-import featureExplorePlus as fep
-
-data_path = "/Users/xiaolin/Desktop/examples.csv"
-pdtt = fep.PandasTools(data_path, sep=",")  # 默认是显示详细内容
-df = pdtt.read_data()  # 这里可以设置只读取部分 nrows=1000
-
-fet = fep.FeatureExplore(tree_split=True)  # 默认不使用决策树分BIN
-# fet.feature_exp_plots(data=df,data_test=df,target_col='label',features_list=['col2','col1'])
-# df=fet.feature_trend_stats(data=df,data_test=df,target_col='label',features_list=['col2','col1'])
+# import featureExplorePlus as fep
+#
+# data_path = "/Users/xiaolin/Desktop/examples.csv"
+# pdtt = fep.PandasTools(data_path, sep=",")  # 默认是显示详细内容
+# df = pdtt.read_data()  # 这里可以设置只读取部分 nrows=1000
+#
+# fet = fep.FeatureExplore(tree_split=True)  # 默认不使用决策树分BIN
+# # fet.feature_exp_plots(data=df,data_test=df,target_col='label',features_list=['col2','col1'])
+# df = fet.feature_trend_stats(data=df.fillna(-1), data_test=df.fillna(-1), target_col='label', features_list=['col2'])
 # print(df.head())
-s=fet.get_tree_bins(df, target_col='label', features_list=['col2', 'col1'])
-print(s)
+# # s=fet.get_tree_bins(df, target_col='label', features_list=['col2', 'col1'])
+#
+# # print(s)
